602ee3de9b73c8561d629ad2ac6786d584575c3a
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-05-11 14:34:42 +0700

    Changed the app name in the header. Commented out Validator Map because it might not be relevant in case of the localnet.

--- apps/explorer/src/pages/home/Home.tsx
+++ apps/explorer/src/pages/home/Home.tsx
@@ -2,19 +2,18 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import clsx from 'clsx';
-import { lazy, Suspense } from 'react';
+import { lazy } from "react";
 
 import { AccountsCardGraph } from '~/components/AccountCardGraph';
 import { Activity } from '~/components/Activity';
+import { ErrorBoundary } from "~/components/error-boundary/ErrorBoundary";
 import { CurrentEpoch, OnTheNetwork } from '~/components/HomeMetrics';
 import { PageLayout } from '~/components/Layout/PageLayout';
-import { SuiTokenCard } from '~/components/SuiTokenCard';
-import { TransactionsCardGraph } from '~/components/TransactionsCardGraph';
-import { ErrorBoundary } from '~/components/error-boundary/ErrorBoundary';
+import { SuiTokenCard } from "~/components/SuiTokenCard";
 import { TopPackagesCard } from '~/components/top-packages/TopPackagesCard';
 import { TopValidatorsCard } from '~/components/top-validators-card/TopValidatorsCard';
-import { useNetwork } from '~/context';
-import { Card } from '~/ui/Card';
+import { TransactionsCardGraph } from "~/components/TransactionsCardGraph";
+import { useNetwork } from "~/context";
 import { TabHeader } from '~/ui/Tabs';
 import { Network } from '~/utils/api/DefaultRpcClient';
 
@@ -26,52 +25,58 @@ function Home() {
 	const [network] = useNetwork();
 	const isSuiTokenCardEnabled = network === Network.MAINNET;
 	return (
-		<PageLayout
-			gradient={{
-				content: (
-					<div
-						data-testid="home-page"
-						className={clsx('home-page-grid-container-top', isSuiTokenCardEnabled && 'with-token')}
-					>
-						<div style={{ gridArea: 'network' }} className="overflow-hidden">
-							<OnTheNetwork />
-						</div>
-						<div style={{ gridArea: 'epoch' }}>
-							<CurrentEpoch />
-						</div>
-						{isSuiTokenCardEnabled ? (
-							<div style={{ gridArea: 'token' }}>
-								<SuiTokenCard />
-							</div>
-						) : null}
-						<div style={{ gridArea: 'transactions' }}>
-							<TransactionsCardGraph />
-						</div>
-						<div style={{ gridArea: 'accounts' }}>
-							<AccountsCardGraph />
-						</div>
-					</div>
-				),
-				size: 'lg',
-			}}
-			content={
-				<div className="home-page-grid-container-bottom">
-					<div style={{ gridArea: 'activity' }}>
-						<ErrorBoundary>
-							<Activity initialLimit={TRANSACTIONS_LIMIT} disablePagination />
-						</ErrorBoundary>
-					</div>
-					<div style={{ gridArea: 'packages' }}>
-						<TopPackagesCard />
-					</div>
-					<div data-testid="validators-table" style={{ gridArea: 'validators' }}>
-						<TabHeader title="Validators">
-							<ErrorBoundary>
-								<TopValidatorsCard limit={10} showIcon />
-							</ErrorBoundary>
-						</TabHeader>
-					</div>
-					<div
+    <PageLayout
+      gradient={{
+        content: (
+          <div
+            data-testid="home-page"
+            className={clsx(
+              "home-page-grid-container-top",
+              isSuiTokenCardEnabled && "with-token"
+            )}
+          >
+            <div style={{ gridArea: "network" }} className="overflow-hidden">
+              <OnTheNetwork />
+            </div>
+            <div style={{ gridArea: "epoch" }}>
+              <CurrentEpoch />
+            </div>
+            {isSuiTokenCardEnabled ? (
+              <div style={{ gridArea: "token" }}>
+                <SuiTokenCard />
+              </div>
+            ) : null}
+            <div style={{ gridArea: "transactions" }}>
+              <TransactionsCardGraph />
+            </div>
+            <div style={{ gridArea: "accounts" }}>
+              <AccountsCardGraph />
+            </div>
+          </div>
+        ),
+        size: "lg",
+      }}
+      content={
+        <div className="home-page-grid-container-bottom">
+          <div style={{ gridArea: "activity" }}>
+            <ErrorBoundary>
+              <Activity initialLimit={TRANSACTIONS_LIMIT} disablePagination />
+            </ErrorBoundary>
+          </div>
+          <div style={{ gridArea: "packages" }}>
+            <TopPackagesCard />
+          </div>
+          <div
+            data-testid="validators-table"
+            style={{ gridArea: "validators" }}
+          >
+            <TabHeader title="Validators">
+              <ErrorBoundary>
+                <TopValidatorsCard limit={10} showIcon />
+              </ErrorBoundary>
+            </TabHeader>
+          </div>
+          {/* <div
 						style={{ gridArea: 'node-map' }}
 						className="min-h-[320px] sm:min-h-[380px] lg:min-h-[460px] xl:min-h-[520px]"
 					>
@@ -80,11 +85,11 @@ function Home() {
 								<ValidatorMap minHeight="100%" />
 							</Suspense>
 						</ErrorBoundary>
-					</div>
-				</div>
-			}
-		/>
-	);
+					</div> */}
+        </div>
+      }
+    />
+  );
 }
 
 export default Home;
