c8d7d163c344efd97190f8cc409071ddde8a1414
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-07-21 16:33:49 +0200

    Added a process management script. Updated readme.

--- README.md
+++ README.md
@@ -1,20 +1,58 @@
 # Local Sui Explorer
 
-A fork of [sui-explorer](https://github.com/MystenLabs/sui-explorer), originally developed by MystenLabs and now discontinued.
+Sui Explorer for your localnet.
 
-The purpose of this fork is to provide Sui Explorer for the local net.
+_A fork of [sui-explorer](https://github.com/MystenLabs/sui-explorer), originally developed by MystenLabs and now discontinued._
 
-## Unique Features
+![Sui Explorer for Localnet: Screenshot](https://repository-images.githubusercontent.com/797627100/052271ab-2ee5-4560-8c24-bda45edb608c)
 
-- Tailored to the localnet
-- No third-party trackers
+## Requirements
 
-## How to Use
+- [Node (>= v20)](https://nodejs.org/en/download/)
 
-The simplest way to use it is through [Local Sui Explorer: Cli](https://github.com/kkomelin/sui-explorer-local)
+## Install
 
-## License & Copyright
+```bash
+# globally
+npm install -g sui-explorer-local@latest
+# or to your project:
+npm install -D sui-explorer-local@latest
+```
 
-&copy; MystenLabs, Apache-2.0
+## Use
+
+Start (on [http://localhost:9001/](http://localhost:9001/)):
+
+```bash
+sui-explorer-local start
+```
+
+Stop:
+
+```bash
+sui-explorer-local stop
+```
+
+Restart:
+
+```bash
+sui-explorer-local restart
+```
+
+## Troubleshot
+
+To display logs, run this command:
+
+```bash
+sui-explorer-local logs
+```
+
+## Usage examples
+
+Sui Explorer Local is integrated into [Sui dApp Starter](https://github.com/kkomelin/sui-dapp-starter) and [Suibase](https://github.com/ChainMovers/suibase).
+
+## License & copyright
+
+Original version of [Sui Explorer](https://github.com/MystenLabs/sui-explorer) - &copy; [MystenLabs](https://github.com/MystenLabs), Apache-2.0
 
 All [changes](./CHANGELOG.md) after Apr 10, 2024 are a subject of copyright of [Konstantin Komelin](https://github.com/kkomelin) and other involved [contributors](https://github.com/kkomelin/sui-explorer/graphs/contributors).
---
+++ index.js
@@ -0,0 +1,162 @@
+#!/usr/bin/env node
+
+import chalk from "chalk";
+import { execSync } from "child_process";
+import { Command } from "commander";
+import { readFileSync } from "node:fs";
+import path from "node:path";
+import pm2 from "pm2";
+import { fileURLToPath } from "url";
+
+const APP_NAME = "Local Sui Explorer";
+const APP_URL = "http://localhost:9001";
+const REPORT_ISSUE_URL = "https://github.com/kkomelin/sui-explorer/issues/new";
+
+const main = async () => {
+  // Commands.
+
+  const program = new Command();
+
+  program
+    .name("sui-explorer-local")
+    .description(`Easily manage ${APP_NAME}`)
+    .version(_getPackageVersion());
+
+  program
+    .command("start")
+    .description(`Start ${APP_NAME}`)
+    .option("-v, --verbose", "display logs")
+    .action((options) => {
+      pm2.connect(function (err) {
+        if (err) {
+          _displayErrorMessage(`Could not start ${APP_NAME}`);
+          options.verbose && err && console.error(err);
+          process.exit(1);
+        }
+
+        pm2.start(
+          {
+            name: "sui-explorer-local",
+            script: "serve",
+            env: {
+              PM2_SERVE_PATH: "./apps/explorer/build/",
+              PM2_SERVE_PORT: 9001,
+              PM2_SERVE_SPA: "true",
+            },
+          },
+          (err, apps) => {
+            pm2.disconnect();
+
+            if (err) {
+              _displayErrorMessage(`Could not start ${APP_NAME}`);
+              options.verbose && console.error(err);
+              process.exit(1);
+              // throw err;
+            }
+          }
+        );
+      });
+
+      _displaySuccessMessage(`${APP_NAME} started on ${APP_URL}`);
+    });
+
+  program
+    .command("stop")
+    .description(`Stop ${APP_NAME}`)
+    .option("-v, --verbose", "display logs")
+    .action((options) => {
+      pm2.connect(function (err) {
+        if (err) {
+          _displayErrorMessage(`Could not stop ${APP_NAME}`);
+          options.verbose && err && console.error(err);
+          process.exit(1);
+        }
+
+        pm2.stop("sui-explorer-local", (err, apps) => {
+          pm2.disconnect();
+
+          if (err) {
+            // throw err;
+            _displayErrorMessage(`Could not stop ${APP_NAME}`);
+            options.verbose && console.error(err);
+            process.exit(1);
+          }
+        });
+      });
+
+      _displaySuccessMessage(`${APP_NAME} stopped`);
+    });
+
+  program
+    .command("restart")
+    .description(`Restart ${APP_NAME}`)
+    .option("-v, --verbose", "display logs")
+    .action((options) => {
+      pm2.connect(function (err) {
+        if (err) {
+          _displayErrorMessage(`Could not restart ${APP_NAME}`);
+          options.verbose && err && console.error(err);
+          process.exit(1);
+        }
+
+        pm2.restart("sui-explorer-local", (err, apps) => {
+          pm2.disconnect();
+
+          if (err) {
+            _displayErrorMessage(`Could not restart ${APP_NAME}`);
+            options.verbose && err && console.error(err);
+            process.exit(1);
+            // throw err;
+          }
+        });
+      });
+
+      _displaySuccessMessage(`${APP_NAME} restarted on ${APP_URL}`);
+    });
+
+  program
+    .command("logs")
+    .description(`Display logs for ${APP_NAME}`)
+    .action((options) => {
+      execSync(`npx pm2 logs sui-explorer-local --nostream`, {
+        stdio: "inherit",
+      });
+    });
+
+  program.parse();
+};
+
+const _getCliDirectory = () => {
+  const currentFileUrl = import.meta.url;
+  return path.dirname(decodeURI(fileURLToPath(currentFileUrl)));
+};
+
+const _getPackageVersion = () => {
+  try {
+    const packageFile = readFileSync(
+      path.join(_getCliDirectory(), "/package.json"),
+      "utf8"
+    );
+    const packageMeta = JSON.parse(packageFile);
+    return packageMeta.version;
+  } catch (e) {
+    _displayErrorMessage(
+      `Cannot read package meta-data. Please report the issue ${REPORT_ISSUE_URL}`
+    );
+    console.error(e);
+    process.exit(1);
+  }
+};
+
+const _displayErrorMessage = (message) => {
+  console.error(chalk.red(message));
+};
+
+const _displaySuccessMessage = (message) => {
+  console.log(chalk.green(message));
+};
+
+// Main entry point.
+main().catch((e) => {
+  console.error(chalk.red(e));
+});
